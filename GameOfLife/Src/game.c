a16e175381957b061d1c65f16424c3a0652da578
Edgar <32952693+Yongyiphan@users.noreply.github.com>
2022-09-25 22:12:13 +0800

    Game can run. Still Need implement function for user input

--- GameOfLife/Src/game.c
+++ GameOfLife/Src/game.c
@@ -3,10 +3,10 @@
 #include <utils.h>
 
 
-#define GOL_GRID_COLS 30
-#define GOL_GRID_ROWS 30
+#define GOL_GRID_COLS 40
+#define GOL_GRID_ROWS 40
 #define GOL_GRID_BUFFERS 2
-#define CELL_LENGTH 15
+#define CELL_LENGTH 10
 
 #define GOL_ALIVE 1
 #define GOL_DEAD 0
@@ -22,8 +22,7 @@ int gGrids[GOL_GRID_BUFFERS][GOL_GRID_ROWS][GOL_GRID_COLS];
 //width and height of each cell
 CP_Color DeadColor, AliveColor;
 //display grid, reference grid
-int dgrid, rgrid;
-
+int rGrid = 0, dGrid = 1;
 /* Feel free to declare your own variables here */
 
 
@@ -56,8 +55,8 @@ void game_init(void)
 
     /* We start unpaused */
     gIsPaused = FALSE;
-    dgrid = 0;
 
+    
     /* Initialization of your other variables here */
 }
 
@@ -65,7 +64,7 @@ void game_update(void)
 {
 
     //Cal Cells
-
+    //CP_Graphics_ClearBackground(DeadColor);
     if (CP_Input_KeyTriggered(KEY_SPACE)) {
         gIsPaused = gIsPaused == FALSE ? TRUE : FALSE;
     }
@@ -77,31 +76,57 @@ void game_update(void)
         //using rgrid, check rules
         //modify dgrid. swap rgrid, dgrid values
         //Any live cell with fewer than two live neighbors dies (emulates underpopulation).
-		//Any live cell with two or three live neighbors lives on.
-		//Any live cell with more than three live neighbors dies (emulates overpopulation).
-		//Any dead cell with exactly three live neighbors becomes a live cell (emulates reproduction).
-        
-        for (int r = 0; r <= GOL_GRID_ROWS; r++) {
-            for (int c = 0; c <= GOL_GRID_COLS; c++) {
-                
+        //Any live cell with two or three live neighbors lives on.
+        //Any live cell with more than three live neighbors dies (emulates overpopulation).
+        //Any dead cell with exactly three live neighbors becomes a live cell (emulates reproduction).
+        CP_Settings_StrokeWeight(1.0f);
+        for (int r = 0; r < GOL_GRID_ROWS; r++) {
+            for (int c = 0; c < GOL_GRID_COLS; c++) {
+                //check for neighbours
+                //if at corners
+                //-> corners == if r == 0, r == ROWs, c == 0 
+                //int left, right, up, down;
+                int count = 0;
+                for (int i = r - 1; i <= r + 1; i++) {
+                    for (int j = c - 1; j <= c + 1; j++) {
+                        if ((i == r && j == c) || (i < 0 || j < 0) || (i >= GOL_GRID_ROWS || j >= GOL_GRID_COLS)) {
+                            continue;
+                        }
+                        if (gGrids[rGrid][i][j] == GOL_ALIVE) {
+                            count++;
+                        }
+                    }
+                }
+                if (gGrids[rGrid][r][c] == GOL_ALIVE) {
+                    if (count < 2) {
+                        gGrids[dGrid][r][c] = GOL_DEAD;
+                    }
+                    else if (count == 2 || count == 3) {
+                        gGrids[dGrid][r][c] = GOL_ALIVE;
+                    }
+                    else if (count > 3) {
+                        gGrids[dGrid][r][c] = GOL_DEAD;
+                    }
+                }
+                else if (count == 3) {
+                    gGrids[dGrid][r][c] = GOL_ALIVE;
+                }
             }
         }
-
-
-        for (int r = 0; r <= GOL_GRID_ROWS; r++) {
-            for (int c = 0; c <= GOL_GRID_COLS; c++) {
-                if (gGrids[dgrid][r][c] == GOL_DEAD) {
-                    CP_Settings_Fill(DeadColor);
-                }
-                else if (gGrids[dgrid][r][c] == GOL_ALIVE) {
+        for (int r = 0; r < GOL_GRID_ROWS; r++) {
+            for (int c = 0; c < GOL_GRID_COLS; c++) {
+                CP_Settings_Fill(DeadColor);
+                if (gGrids[dGrid][r][c] == GOL_ALIVE) {
                     CP_Settings_Fill(AliveColor);
                 }
+                gGrids[rGrid][r][c] = gGrids[dGrid][r][c];
                 CP_Graphics_DrawRect(r * CELL_LENGTH, c * CELL_LENGTH, CELL_LENGTH, CELL_LENGTH);
-
             }
         }
     }
-    printf("%d", gIsPaused);
+    
+    rGrid = rGrid == 1 ? 0 : 1;
+    dGrid = dGrid == 1 ? 0 : 1;
 
 }
 
